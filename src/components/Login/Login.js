1efa53108af2c715573913c45dd7bb7c5d60ef56
Osama alsaaydeh <108791617+Saaideh-Osama@users.noreply.github.com>
2025-03-14 14:07:07 +0300

    login and signup styled

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,98 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import {  MDBBtn,  MDBContainer,  MDBRow,  MDBCol,  MDBInput}
+from 'mdb-react-ui-kit';
+import axios from "axios";
+import logo from '../../assets/images/loginlogo.png';
+import "./Login.css";
 
 function Login() {
-  return (
-    <div className='Login-container'>
-        <form className='Login-form'>
-            <h1>Login</h1>
-            <input type='text' placeholder='Username' className='login-inputs' />        
-            <input type='password' placeholder='Password' className='login-inputs'/>
-            <button type='submit' id='login-button' className='login-inputs'>Login</button>
-            <p>Don't have an account? <a href='/signup'>Sign up</a></p>
-            
-            </form>
 
+ //Context-object
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+ 
 
+ const handleSubmit = async (e) => {
 
-
-    </div>
-  )
+    e.preventDefault();
+  try{
+    const response = await axios.post(
+      "https://your-ngrok-url.ngrok.io/api/login",
+      {
+        email,
+        password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }
+    );
+    console.log("Login Successful", response.data);
+  }
+  catch(err){
+    console.error("Login Error", err);
+    setError(err.response?.data?.message || "Login failed");
 }
+  }
 
-export default Login
+
+  return (
+    <MDBContainer className="mt-3 gradient-form  vh-90" style={{maxHeight: '90vh',maxWidth:'70vw',borderRadius: '0px',backgroundColor:'#d0c6bd'}}/*the biggest container*/ > 
+
+    <MDBRow  className="h-100" >
+
+      <MDBCol col='6' className="mb-5  "  style={{maxHeight: '90vh',maxWidth:'35vw'}}>
+        <form className="d-flex flex-column ms-5" onSubmit={handleSubmit}>
+
+          <div className="text-center">
+            <img src={logo}
+              style={{width: '185px'}} alt="logo" />
+
+          </div>
+
+          <p>Please login to your account</p>
+
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'  onChange={(e) => setEmail(e.target.value)} />
+          <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' onChange={(e) => setPassword(e.target.value)}/>
+
+
+          <div className="text-center pt-1 mb-5 pb-1">
+            <MDBBtn className="mb-4 w-100 gradient-custom-2 text-white" style={{maxHeight: '6vh',maxWidth:'10vw', backgroundColor:'#54473F',borderWidth:'0px' }} >Sign in</MDBBtn>
+            
+          </div>
+
+          <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
+            <p className="mb-0 mt-0 pt-0 text-black">Don't have an account?</p>
+            <MDBBtn outline className='mx-2' color=''  style={{maxHeight: '6vh',maxWidth: '8vw',backgroundColor:'#54473F',borderWidth:'0px'}} >
+              <a href="/signup" className="text-white" style={{textDecoration: 'none',fontColor: '#41644A'}} >Register</a>
+            </MDBBtn>
+          </div>
+
+        </form>
+
+      </MDBCol>
+
+      <MDBCol col='6' className="mb-5" style={{ paddingRight: '0px'}} >
+        <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-95 mb-4 h-100 " style={{borderRadius: '0px' , paddingRight: '0px' ,fontColor: '#3A4F7A'}}>
+          <div className=" px-0 py-4 p-md-5 mx-md-4 h-100  "  style={{borderRadius: '0px' , paddingRight: '0px' ,fontColor: '#332c28'}}>
+            <h4 class="mb-4" >We are more than just a company</h4>
+            <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+
+        </div>
+
+      </MDBCol>
+
+    </MDBRow>
+
+  </MDBContainer>
+  );}
+
+export default Login;
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,19 +1,107 @@
-import React from 'react'
-import './Signup.css'
+import React, { useState } from 'react'
+import logo from '../../assets/images/loginlogo.png';
+import axios from 'axios';
+import { MDBInput,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBBtn,
+    MDBCol,
+    MDBContainer, 
+    MDBRow
+} from "mdb-react-ui-kit"; 
+
+
+
+  
+
 function Signup() {
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+    const [password, setPassword] = useState("");
+    const [selectedValue, setSelectedValue] = useState("Select Diet");
+    const [error, setError] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+
+        const handleSelect = (value) => { setSelectedValue(value);};
+        
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            setError("");
+            setSuccessMessage("");
+    
+            try {
+                const response = await axios.post("https://your-ngrok-url.ngrok.io/api/signup", {
+                    email,
+                    name,
+                    phone_number: number,
+                    password,
+                    diet_preference: selectedValue
+                }, {
+                    headers: { "Content-Type": "application/json" }
+                });
+    
+                console.log("Signup Successful", response.data);
+                setSuccessMessage("Signup successful! You can now log in.");
+            } catch (err) {
+                console.error("Signup Error", err);
+                setError(err.response?.data?.message || "Signup failed. Please try again.");
+            }
+        };
+
+
   return (
-    <div id='signup-container'>signup
-<form id='signup-form'>
-    <h1>Sign Up</h1>
-    <input type='email' placeholder='Email' className='signup-inputs' />
-    <input type='password' placeholder='Password' className='signup-inputs' />
-    <button type='submit' id='signup-button' className='signup-inputs'>Sign Up</button>
-    <p>Already have an account? <a href='/'>Login</a></p>
-</form>
+<MDBContainer className='  d-flex justify-content-center align-content-center my-3'  style={{maxWidth:'70vw',borderRadius: '0px',backgroundColor:'#d0c6bd'}}/*the biggest container*/ >
+            <form  className='d-flex flex-column' onSubmit={handleSubmit}>
+
+    <MDBRow className='' >
+            
+        <MDBCol className='d-flex flex-column align-content-center justify-content-evenly px-5 ' col='6' >
+        <h2>Sign up</h2>
+        <MDBInput wrapperClass='mb-2' label='Email address' type='email'  onChange={(e) => setEmail(e.target.value)} />
+        <MDBInput wrapperClass='mb-2' label='name' type='text' onChange={(e) => setName(e.target.value)}/>
+        <MDBInput wrapperClass='mb-2' label='phone-Number' type='number' onChange={(e) => setNumber(e.target.value)}/>
+        <MDBInput wrapperClass='mb-2' label='Password' type='password' onChange={(e) => setPassword(e.target.value)}/>
+       
+        <MDBDropdown>
+        <MDBDropdownToggle tag='a' className='btn btn-warning text-black'  onChange={(e) => handleSelect(e.target.value)}>
+        {selectedValue}
+        </MDBDropdownToggle>
+        <MDBDropdownMenu>
+        <MDBDropdownItem  link onClick={() => handleSelect("Vegetarian")}>Vegetarian</MDBDropdownItem>
+        <MDBDropdownItem link onClick={() => handleSelect("Vegan")}>vegan</MDBDropdownItem>
+        <MDBDropdownItem link  onClick={() => handleSelect("Omnivore")}>omnivore</MDBDropdownItem>
+        <MDBDropdownItem  link onClick={() => handleSelect("lactose intolerant")}>lactose intolerant</MDBDropdownItem>
+        <MDBDropdownItem  link onClick={() => handleSelect("gluten free")}>gluten free</MDBDropdownItem>
+        <MDBDropdownItem  link onClick={() => handleSelect("diabetic")}>diabetic</MDBDropdownItem>
+        </MDBDropdownMenu>
+        </MDBDropdown>
+            <div className="text-center pt-2 mb-3 pb-1">
+                <MDBBtn className="mb-4 w-100 gradient-custom-2 text-white" style={{maxHeight: '6vh',maxWidth:'10vw', backgroundColor:'#54473F',borderWidth:'0px' }} >Sign up</MDBBtn>
+            </div>
+    <div className='text-center pt-1 mb-3 pb-1'>Already have an account? <a href='/'>Login</a></div>
+        </MDBCol>
+        <MDBCol col='6' className="mb-5 gradient-custom-2  d-flex flex-column justify-content-center align-content-centers" style={{ paddingRight: '0px'}} >
+            <img src={logo} style={{width:'150px',height:'150px'}}/>
+            <div className="d-flex flex-column  justify-content-center  h-95 mb-4 h-100 " style={{fontColor: '#3A4F7A'}}>
+                    <div className=" px-0 py-4 p-md-5 mx-md-4 h-100  "  style={{borderRadius: '0px' , paddingRight: '0px' ,fontColor: '#332c28'}}>
+                    <h4 className="mb-4" >We are more than just a company</h4>
+                    <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+    
+        </MDBCol>
+    </MDBRow>
+    </form>
+</MDBContainer>
 
 
-
-    </div>
     )
 }
 
